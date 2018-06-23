e2595887eb508f1fca5071f01d9379a78b33cf4d
Vicko Franić <vicko@q-alliance.com>
2018-06-23 20:01:56 +0200

    Removed web profiler, added var dumper

--- config/bundles.php
+++ config/bundles.php
@@ -10,6 +10,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
